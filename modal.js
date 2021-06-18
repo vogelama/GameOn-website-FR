8057cc419654f7f5201aa965e46953c23cac2aeb
Amanda Vogel <vogelama@gmail.com>
2021-06-18 15:57:02 +0200

    TODO # 1: close modal- Add functionality to button (x)

--- modal.js
+++ modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeForm = document.querySelectorAll(".close"); 
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,6 +20,15 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+//launch modal close event
+closeForm.forEach((btn) => btn.addEventListener("click", closeModal));
+
+// launch modal close form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
 
 
 
