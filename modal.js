962d12910b025c023f5abd8f4221262d468644c4
Amanda Vogel <vogelama@gmail.com>
2021-06-24 17:00:34 +0200

    TODO # 3: Add validation or error messages (updated)

--- modal.js
+++ modal.js
@@ -8,7 +8,7 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
+//////////////////////////////////////////////////////////////
 
 /* OPENING FORM */
 // targeting form as a whole
@@ -34,102 +34,150 @@ closeForm.forEach((btn) => btn.addEventListener("click", closeModal));
 function closeModal(){
   modalbg.style.display = "none";
 }
-
+//////////////////////////////////////////////////////////////
 
 /* VALIDATING THE FORM */
-/* CONST VALUES FOR FORM AND INPUTS */
-const form = document.getElementById('form');
-const first = document.getElementById("first");
-const last = document.getElementById("last");
-const email = document.getElementById("email");
-const birthdate = document.getElementById("birthdate");
-const quantity = document.getElementById("quantity");
+/* Using document query selector to select the input fields and the form */
+const first = document.querySelector('#first');
+const last = document.querySelector('#last');
+const email = document.querySelector('#email');
+const birthdate = document.querySelector('#birthdate');
+const quantity = document.querySelector('#quantity');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+const form = document.querySelector('#reserve');
 
-  checkInputs();
+
+/* attaching the submit event listener to the form using addEventListener */
+form.addEventListener('submit', function validate(e) {
+
+  // validate input fields 
+  let isFirstValid = checkFirst(),
+      isLastValid = checkLast(),
+      isEmailValid = checkEmail(),
+      isBirthdateValide = checkBirthdate(),
+      isQuantityValid = checkQuantity();
+
+  // validate form 
+  let isFormValid = 
+      isFirstValid &&
+      isLastValid &&
+      isEmailValid &&
+      isBirthdateValide &&
+      isQuantityValid;
+
+  //don't submit to the server if form is invalid 
+  if (!isFormValid) {
+      // prevent the form from submitting
+      e.preventDefault(); 
+  }
 });
 
 
-/* CHECKING INPUTS TO SEE IF THEY FOLLOW THE RULES AND GET VALIDATED OR ARE INCORRECT AND GET ERROR MESSAGE */
-function checkInputs() {
-  // get the values from the inputs 
+/* Utility function for email */
+const isEmailValid = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+};
+
+
+/* Functions that show the error / success message */
+const showError = (input, message) => {
+  // get the formData element
+  const formData = input.parentElement;
+  // add the error class
+  formData.classList.remove('success');
+  formData.classList.add('error');
+
+  // show the error message
+  const error = formData.querySelector('small');
+  error.textContent = message; 
+};
+const showSuccess = (input) => {
+  // get the formData element
+  const formData = input.parentElement;
+  // remove the error class
+  formData.classList.remove('error');
+  formData.classList.add('success');
+
+  // hide the error message
+  const error = formData.querySelector('small');
+  error.textContent = ''; 
+};
+
+
+/* Input field validating functions */
+/* Validate the first name field */
+const checkFirst = () => {
+
+  let valid = false;
   const firstValue = first.value.trim();
+
+  if (firstValue.length < 2) {
+    showError(first, 'Au moins 2 lettres');
+  } else {
+    showSuccess(first);
+    valid = true; 
+  }
+  return valid;
+};
+/* Validate the last name field */
+const checkLast = () => {
+
+  let valid = false;
   const lastValue = last.value.trim();
+
+  if (lastValue.length < 2) {
+    showError(last, 'Au moins 2 lettres');
+  } else {
+    showSuccess(last);
+    valid = true; 
+  }
+  return valid;
+};
+/* Validate the email field */
+const checkEmail = () => {
+
+  let valid = false;
   const emailValue = email.value.trim();
+
+  if (emailValue === '') {
+    showError(email, 'Email est invalide');
+  } else if (!isEmailValid(emailValue)) {
+    showError(email, 'Email est invalide');
+  } else {
+    showSuccess(email);
+    valid = true; 
+  }
+  return valid;
+};
+/* Validate the birthdate field */
+const checkBirthdate = () => {
+
+  let valid = false;
   const birthdateValue = birthdate.value.trim();
+
+  if (birthdateValue === '') {
+    showError(birthdate, 'Mettez date de naissance');
+  } else {
+    showSuccess(birthdate);
+    valid = true; 
+  }
+  return valid;
+};
+/* Validate the tournament quantity field */
+const checkQuantity = () => {
+
+  let valid = false;
   const quantityValue = quantity.value.trim();
 
-  if(firstValue.length < 2) {
-    // add error class
-    setErrorFor(first, 'Au moins 2 lettres');
+  if (quantityValue === '' || quantityValue > 99) {
+    showError(quantity, 'Doit être un numéro entre 0 et 99');
   } else {
-    // add success class 
-    setSuccessFor(first);
+    showSuccess(quantity);
+    valid = true; 
   }
-
-  if(lastValue.length < 2) {
-    // add error class
-    setErrorFor(last, 'Au moins 2 lettres');
-  } else {
-    // add success class 
-    setSuccessFor(last);
-  }
-
-  if(emailValue === '') {
-    // add error class
-    setErrorFor(email, 'Ne laissez pas vide');
-  } else if(!isEmail(emailValue)) {
-    setErrorFor(email, 'E-mail est invalide');
-  } else {
-    // add success class 
-    setSuccessFor(email);
-  }
-
-  if(birthdateValue === '') {
-    // add error class
-    setErrorFor(birthdate, 'Ne laissez pas vide');
-  } else {
-    // add success class 
-    setSuccessFor(birthdate);
-  }
-
-  if(quantityValue === '' || quantityValue > 99) {
-    // add error class
-    setErrorFor(quantity, 'Doit être un numéro entre 0 et 99');
-  } else {
-    // add success class 
-    setSuccessFor(quantity);
-  }
-
-  // success message if everything is successful 
-}
-
-
-/* CUSTOM FUNCTION TO CHECK EMAIL VALIDITY */
-function isEmail(email) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
-
-
-/* FUNCTIONS TO SET ERROR MESSAGE OR SET INPUT AS SUCCESSFUL */
-function setErrorFor(input, message) {
-  const formData = input.parentElement; //.formData
-  const small = formData.querySelector('small'); //<small>
-
-  // add error message inside small element
-  small.innerText = message;
-  // add error class
-  formData.className = 'formData error';
-}
-
-function setSuccessFor(input) {
-  const formData = input.parentElement; //.formData
-
-  // add success class
-  formData.className = 'formData success';
-}
+  return valid;
+};
 
 
 
@@ -146,48 +194,33 @@ function setSuccessFor(input) {
 
 
 
-// //Error Messages input elements
-// //function to validate all input fields 
-// function validate(event) {
-//   event.preventDefault();
-//   validateFirstName();
-//   validateLastName();
-//   validateEmail();
-//   validateBirthdate();
-//   validateQuantity();
-// }
 
-// //if input fields are incorrect call error message for each incorrect input 
-// function validateFirstName() {
-//   if(first.value.length < 2) { //more than 2 characters
-//     displayErrorMessage(first, "Au moins 2 lettres");
-//   } 
-// }
-// function validateLastName() {
-//   if(last.value.length < 2) { //more than 2 characters
-//     displayErrorMessage(last, "Au moins 2 lettres");
-//   }
-// }
-// function validateEmail() {
-//   // var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-//   if(email.value === '') { //must be a valid email
-//     displayErrorMessage(email, "L'email est invalide");
-//   }
-// }
-// function validateBirthdate() {
-//   if(birthdate.value === '') { //is empty 
-//     displayErrorMessage(birthdate, "Ne laissez pas vide");
-//   }
-// }
-// function validateQuantity() {
-//   if(quantity.value === '') { //a numeric value between 0 and 99
-//     displayErrorMessage(quantity, "Doit être un numéro entre 0 et 99");
-//   }
-// }
-// //function to display error message
-// function displayErrorMessage(input, message){
-//   input.parentElement.querySelector('small').textContent = message;
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
